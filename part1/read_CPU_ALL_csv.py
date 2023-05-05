839f2682fc572ab9ab31e464601664b422943529
akshaypa <akshaypa@maveric-systems.com>
2023-05-05 11:03:19 +0530

    Added the functionality to collect the CPU_ALL and MEM data from the txt file

--- part1/read_CPU_ALL_csv.py
+++ part1/read_CPU_ALL_csv.py
@@ -1,10 +1,11 @@
 import glob
 import csv
 
-sumc = 0
-CPUList={""}
-MEMList={""}
-file_path="C:/Users/manimalak/PycharmProjects/pythonProject/csv File/data.txt"
+
+CPUList = list()
+MEMList = list()
+#file_path="C:/Users/manimalak/PycharmProjects/pythonProject/csv File/data.txt"
+file_path = "C:/Users/akshaypa/PycharmProjects/python/data.txt"
 
 with open(file_path,"r")as file:
     contents = file.read()
@@ -13,14 +14,10 @@ with open(file_path,"r")as file:
 cpu_files=open("data.csv")
 for line in cpu_files:
     if line.startswith("CPU_ALL"):
-        line.lstrip(line,'CPU_ALL')
-
-        sumc = sumc + 1
+        CPUList.append(line.replace("CPU_ALL,", ''))
     elif line.startswith("MEM"):
-        MEMlist
-print(sumc)
+        MEMList.append(line.replace("MEM,", ''))
+print("Data from CPU is as follows: ", CPUList)
+print("Data from MEM is as follows: ", MEMList)
 
 
-#search for all files in the current directory that end in .txt
-#cpu_files=glob.glob("data.txt/CPU_ALL*")
-#print(cpu_files)
