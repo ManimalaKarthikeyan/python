06503cf88049ed6855f515389cf003991da0053a
akshaypa <akshaypa@maveric-systems.com>
2023-05-05 11:16:49 +0530

    Added the functionality to collect the CPU_ALL and MEM data from the txt file

--- part1/read_CPU_ALL_csv.py
+++ part1/read_CPU_ALL_csv.py
@@ -1,17 +1,22 @@
 import glob
 import csv
 
-
+# two empty list to store the required data
 CPUList = list()
 MEMList = list()
+
+#file path
 #file_path="C:/Users/manimalak/PycharmProjects/pythonProject/csv File/data.txt"
 file_path = "C:/Users/akshaypa/PycharmProjects/python/data.txt"
 
+#reading contents
 with open(file_path,"r")as file:
     contents = file.read()
     file.close()
     #print(contents)
 cpu_files=open("data.csv")
+
+#looping to get desired section
 for line in cpu_files:
     if line.startswith("CPU_ALL"):
         CPUList.append(line.replace("CPU_ALL,", ''))
